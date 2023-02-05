1b166853455ea8a7f9b53e509d5df666c4fe26b2
Abhishek Singh <abhishek.singh@Abhishek-Acko2023.local>
2023-02-05 15:21:53 +0530

    fixes

--- src/main/java/com/docker/backenddeployment/controller/server.java
+++ src/main/java/com/docker/backenddeployment/controller/server.java
@@ -2,11 +2,9 @@ package com.docker.backenddeployment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
 public class server {
     @GetMapping("server")
     public String server() {
