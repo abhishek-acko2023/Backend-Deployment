85e5fb3dd8f02fdfa2b636dc8c9154824e331026
Abhishek Singh <abhishek.singh@Abhishek-Acko2023.local>
2023-02-05 14:52:14 +0530

    fixes

--- src/main/java/com/docker/backenddeployment/BackendDeploymentApplication.java
+++ src/main/java/com/docker/backenddeployment/BackendDeploymentApplication.java
@@ -8,6 +8,7 @@ public class BackendDeploymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendDeploymentApplication.class, args);
+		System.out.println("Server is Running!");
 	}
 
 }
